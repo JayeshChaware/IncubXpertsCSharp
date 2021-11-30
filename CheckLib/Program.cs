c06ed4d1b9981737177e995c08d61cd4c3d24238
Jayesh <jchaware4@gmail.com>
2021-11-30 14:31:34 +0530

    check commit

---
+++ CheckLib/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Calculator;
+namespace CheckLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(TwoNumCal.TwoNumSum(10, 25));
+            Console.WriteLine(ThreeNumCal.ThreeNumSum(10,21, 25));
+            Console.WriteLine(TwoNumCal.TwoNumProduct(10, 25));
+            Console.WriteLine(TwoNumCal.TwoNumSubtraction( 25,10));
+            Console.WriteLine(TwoNumCal.NumMod( 25,10));
+            Console.WriteLine(TwoNumCal.TwoNumDivision(100, 25));
+        }
+    }
+}
