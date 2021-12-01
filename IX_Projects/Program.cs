4223d11ebd8d955dd809d74bb699e7cdb33c9ff0
JayeshChaware <jayesh.chaware-imed@bvp.edu.in>
2021-12-02 02:05:29 +0530

    animal and youtube class

--- IX_Projects/Program.cs
+++ IX_Projects/Program.cs
@@ -8,7 +8,15 @@ namespace IX_Projects
         {
            
             Subscriber subscriber = new Subscriber();
-            
+            Bike Apache160_4V = new Bike("TVS", 160, 2, 2);
+            Bike Apache200_4V = new Bike("TVS", 200, 2, 2);
+
+            Car Swift = new Car("Maruti Suzuki", 1200, 4, 5);
+            Car Celerio = new Car("Maruti Suzuki", 1000, 4, 5);
+            Apache160_4V.ViewBike();
+            Apache200_4V.ViewBike();
+            Swift.ViewCar();
+            Celerio.ViewCar();
         }
     }
 }
---
+++ IX_Projects/vehicle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IX_Projects
+{
+    public class Vehicle
+    {
+        public string Company { get; set; }
+        public bool Start { get; set; }
+        public float Speedometer { get; set; }
+        public int Engine { get; set; }
+        
+    }
+
+    class Car : Vehicle 
+    {
+        public int Wheels { get; set; }
+        public int Seat { set; get; }
+        public string Type { get; set; }
+        public Car(string company, int engine, int wheels, int seat)
+        {
+            string company1 = company;
+            Company = company1;
+            Engine = engine;
+            Wheels = wheels;
+            Seat = seat;
+        }
+        public void ViewCar()
+        {
+            Console.WriteLine("Car brand is {0}, Engine is {1}cc, it has {2} wheels, it is {3} seater", Company, Engine, Wheels, Seat);
+
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public int Wheels { get; set; }
+        public int Seat { set; get; }
+        public string Type { get; set; }
+        public Bike(string company, int engine, int wheels, int seat)
+        {
+            string company1 = company;
+            Company = company1;
+            Engine = engine;
+            Wheels = wheels;
+            Seat = seat;
+        }
+        public void ViewBike()
+        {
+            Console.WriteLine("Bike brand is {0}, Engine is {1}cc, it has {2} wheels, it is {3} seater", Company, Engine, Wheels, Seat);
+
+        }
+    }
+    class Pickup : Vehicle
+    { 
+        public int Capacity { get; set; }
+        public int Wheels { get; set; }
+        public int Seat { set; get; }
+        public Pickup(string company, int engine, int capacity, int wheels, int seat) {
+            string company1 = company;
+            Company = company1;
+            Engine = engine;
+            Capacity = capacity;
+            Wheels = wheels;
+            Seat = seat;
+        }
+        public void ViewPickup()
+        {
+            Console.WriteLine("pickup truck brand is {0}, Engine is {1}cc, pickup capacity is {2}, it has {3} wheels, it is {4} seater",Company,Engine,Capacity,Wheels,Seat);
+        }
+
+
+
+    }
+
+}
